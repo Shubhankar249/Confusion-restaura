86d0e38ca420ee6570a2e8f43a981b08c21374d6
Shubhankar <shubhankar.andi5@gmail.com>
2020-06-16 12:01:10 +0530

    Components part-1

--- App.js
+++ App.js
@@ -1,19 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Main from "./components/MainComponent";
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello Shubhankar!!</Text>
-    </View>
+    <Main />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
