e79d171eee2a543afb2e09956c6c813d7a93ab02
Shubhankar <shubhankar.andi5@gmail.com>
2020-07-06 13:31:27 +0530

    redux-persist

--- redux/configureStore.js
+++ redux/configureStore.js
@@ -1,17 +1,31 @@
-import {applyMiddleware, combineReducers, createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-import {dishes} from './dishes';
-import {comments} from './comments';
-import {promotions} from './promotions';
-import {leaders} from './leaders';
-import {favourites} from "./favourites";
+import { dishes } from './dishes';
+import { comments } from './comments';
+import { promotions } from './promotions';
+import { leaders } from './leaders';
+import { favourites } from './favourites';
 
-export const ConfigureStore= () => {
-    return createStore(
-        combineReducers({
-            dishes, comments, leaders, promotions, favourites
+import {persistCombineReducers,persistStore} from 'redux-persist'
+import {AsyncStorage} from 'react-native'
+
+export const ConfigureStore = () => {
+    const config = {
+        key:'root',
+        storage:AsyncStorage,
+        debug:true
+    };
+    const store = createStore(
+        persistCombineReducers(config,{
+            dishes,
+            comments,
+            promotions,
+            leaders,
+            favourites
         }),
         applyMiddleware(thunk, logger)
     );
+    const persistor=persistStore(store);
+    return {persistor,store};
 };
